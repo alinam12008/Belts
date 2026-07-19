f1264bc43c633480fff3d3cc7c2585710b9af60a
alinam12008 <alinamomin2008@gmail.com>
2026-07-19 21:41:21 +0530

    Update

--- index.js
+++ index.js
@@ -84,6 +84,12 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+app.use((req, res, next) => {
+  res.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+  res.set('Pragma', 'no-cache');
+  res.set('Expires', '0');
+  next();
+});
 
 // [FIX] Middleware to wait for DB readiness
 const ensureDbReady = async (req, res, next) => {
